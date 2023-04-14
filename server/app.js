af122655938cd7c09fbf04ce9a36de7fc6e8763c
borel-T <borel-T@users.noreply.github.com>
2023-04-14 19:55:12 +0300

    added mutations

--- server/app.js
+++ server/app.js
@@ -1,20 +1,27 @@
 const express = require("express");
-
+const moogoose = require("mongoose");
+require("dotenv/config");
 const app = express();
 const { graphqlHTTP } = require("express-graphql"); // would be use as middleware to intercept gQl requests
 
 // get our schema
-const schema = require("./schema/index");
+const schema = require("./schema/index"); // schema using hard-coded data
+const schemaWithMongoDB = require("./schema/schemaWithMongoDB"); // schema using mongoDB
+const { default: mongoose } = require("mongoose");
 
 // use graphql as middleware
 // graph ql funtion would be executed at each request
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: schema, // graph-ql schema
+    schema: schemaWithMongoDB, // graph-ql schema
     graphiql: true, // use graphiql for queries to this path
   })
 );
+
+// connect to mongo
+moogoose.connect(process.env.DB_CONNECTION);
+mongoose.connection.once("open", () => [console.log("connection to DB")]);
 
 app.listen(4000, (err) => {
   if (err) {
---
+++ server/schema/schemaWithMongoDB.js
@@ -0,0 +1,140 @@
+const graphql = require("graphql");
+const Book = require("../models/book"); // import DB models
+const Author = require("../models/author"); // import DB models
+
+// use of schema
+// 1- defining object types
+// 2 - relationship btw types
+// 3 - define Rootqueries (root or entry point to the gragh)
+
+// let get some gQL utils for this
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLBoolean,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLList,
+} = graphql;
+
+// using this we would define a new object type (type here = schema of responses)
+const BookType = new GraphQLObjectType({
+  name: "Book",
+  fields: () => ({
+    // define different object fields
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+    allSold: { type: GraphQLBoolean },
+    authorId: { type: GraphQLID },
+    // DEFINE QUERY FOR RELATED SCHEMAS
+    author: {
+      type: AuthorType, // mention it's type
+      // resolve the query (parent here represents the book schema, which is the current parent of the author in this type definition)
+      resolve(parent, args) {},
+    },
+  }), // fields is handle by a fxn inorder to differetiate btw oth
+});
+
+// author type
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    // an author can have more than 1 book
+    // let tell the object type that our author can have a list of books
+    books: {
+      type: new GraphQLList(BookType),
+      // if books is requestedm handle the query data to be returned
+      resolve(parent, args) {},
+    },
+  }),
+});
+
+// root-query (routes or entry points to the graph)
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    // our first root query is querying a particular item (name_of_query, type_of_data)
+    book: {
+      type: BookType, // what kind of object-type woudld be returned
+      args: { id: { type: GraphQLID } }, // args* to identity the object-data
+      resolve(parent, args) {},
+    },
+    // query for athors
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {},
+    },
+    // DEFINE QUERIES routes for listing-queries
+    books: {
+      type: GraphQLList(BookType),
+      resolve(parent, args) {},
+    },
+    authors: {
+      type: GraphQLList(AuthorType),
+      resolve(parent, args) {},
+    },
+  },
+});
+
+// Our mutations
+// mutations allow us to create, update, delete data
+const Mutations = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    // create a mutation action for adding to DB
+    addAuthor: {
+      // which type of data are we adding
+      type: AuthorType,
+      // what are we expecting as data to be stored
+      args: {
+        name: { type: GraphQLString },
+        age: { type: GraphQLInt },
+      },
+      // now let's handle the add request
+      resolve(parent, args) {
+        // mongoose add
+        let author = new Author({
+          name: args.name,
+          age: args.age,
+        });
+        // saving
+        return author.save();
+      },
+    },
+    // create a mutation action for adding to DB
+    addBook: {
+      // which type of data are we adding
+      type: BookType,
+      // what are we expecting as data to be stored
+      args: {
+        name: { type: GraphQLString },
+        genre: { type: GraphQLString },
+        authorId: { type: GraphQLID },
+      },
+      // now let's handle the add request
+      resolve(parent, args) {
+        // mongoose add
+        let book = new Book({
+          name: args.name,
+          genre: args.genre,
+          authorId: args.authorId,
+        });
+        // save and return success
+        return book.save();
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  // our schema has the query definition
+  query: RootQuery,
+  // pass our mutations
+  mutation: Mutations,
+});
