c70cd15bbbad9400b40f604c094298089d118bca
borel-T <borel-T@users.noreply.github.com>
2023-04-15 21:41:47 +0300

    added single gql query

--- client/src/queries/books.js
+++ client/src/queries/books.js
@@ -1,14 +1,34 @@
 import { gql } from "@apollo/client";
 
 export const GET_BOOK = gql`
-  {
-    book(id: "")
+  query ($id: ID!) {
+    book(id: $id) {
+      name
+      genre
+      author {
+        name
+        books {
+          name
+          genre
+        }
+      }
+    }
   }
 `;
 
 export const GET_BOOKS = gql`
   query GetBooks {
     books {
+      id
+      name
+      genre
+    }
+  }
+`;
+
+export const ADD_BOOK = gql`
+  mutation ($name: String!, $genre: String!, $authorId: ID!) {
+    addBook(name: $name, genre: $genre, authorId: $authorId) {
       name
       genre
     }
